4224d34932ec796a1a577a021a182586812594c0
kratos7070 <kratos7070@hotmail.es>
2013-03-21 02:07:56 +0100

    Update Pruebagithub2.java

--- pruebagithub2/src/pruebagithub2/Pruebagithub2.java
+++ pruebagithub2/src/pruebagithub2/Pruebagithub2.java
@@ -15,5 +15,13 @@ public class Pruebagithub2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        
+        System.out.println("hola man");
+        
+        
+        
+        
     }
 }
