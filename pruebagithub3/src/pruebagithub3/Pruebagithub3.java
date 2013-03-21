47fbf65045689cbcdd8d4091e80327b8edecfedb
kratos7070 <kratos7070@hotmail.es>
2013-03-21 02:09:09 +0100

    Update Pruebagithub3.java

--- pruebagithub3/src/pruebagithub3/Pruebagithub3.java
+++ pruebagithub3/src/pruebagithub3/Pruebagithub3.java
@@ -14,6 +14,15 @@ public class Pruebagithub3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        
+        //
+        //
+        
+        
+        System.out.println("hola man de new jejeje");
+        
+        
     }
 }
